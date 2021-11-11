f6df34544e48f5911df31ce857cb27df1812d041
DuyTan <duytannguyen03022003@gmail.com>
2021-11-11 14:41:23 +0700

    Initial commit

---
+++ Baitap2.py
@@ -0,0 +1,7 @@
+a=int(input("a="))
+b=int(input("b="))
+print("tổng 2 số =",a+b)
+print("tích 2 số =",a*b)
+print("hiệu 2 số =",a-b)
+print("phép chia lấy dư của 2 số =",a%b)
+print("phép chia lấy nguyên của 2 sô =",a//b)
